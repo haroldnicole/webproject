1ac0b2042cdb27987bebedc24755ec838422ae43
haroldnicole <nclguiao@gmail.com>
2017-12-26 14:58:01 -0800

    hello

---
+++ helloworld.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>GIT HUB</title>
+</head>
+<body>
+<?php
+echo 'hello world';
+
+	for ($a=1;$a<=5;$a++)
+	{
+	 $str4="<h$a>hello world</h$a>";
+	 echo $str4; 
+	
+	}
+?>
+</body>
+</html>
